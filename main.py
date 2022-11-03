e7c46d3250d0574da9b42f396f038144058c659e
cult_mentality <kavitkarswanand5@gmail.com>
2022-11-02 19:26:55 -0700

    fix for add init and log command

--- main.py
+++ main.py
@@ -10,119 +10,190 @@ from decimal import Decimal
 import sys
 
 # Path to check the blockchain file
-os.environ["BCHOC_FILE_PATH "] = "./"
 
-#we need to implement these functions 
-def append(case_id,item_list):
-	#get prehash value
-	bchoc_file_read=open("bchoc_file.bin","rb")
-	data=bchoc_file_read.read()
-	index=0
-	length=0
-	while index<=(len(data)-1):
-		print(datetime.datetime.fromtimestamp(struct.unpack('d',data[index+32:index+40])[0]))
-		length=struct.unpack('I',data[index+72:index+76])[0]
-		index=index+length+76
-		print("len: ",length)
-		print("index: ",index)
-	bchoc_file_read.close()
-	pre_sha256=hashlib.sha256(data[index-length-76:]).hexdigest()
-	print(pre_sha256)
-	
-	bchoc_file=open("bchoc_file.bin","ab")
-	print("Case: ",case_id)
-	for i in item_list:
-		print(i)
-		pre_hash=bytes(pre_sha256,'utf-8')
-		dt=datetime.datetime.now()
-		time_stamp=dt.timestamp()
-		item_id=int(i)
-		state=bytes("INITIAL",'utf-8')
-		data_length=0
-		bchoc_file.write(struct.pack('32sd16sI12sI',pre_hash,time_stamp,bytes(case_id,'utf-8'),0,state,data_length))
-	bchoc_file.close()
- 
+if not ("BCHOC_FILE_PATH" in os.environ):
+    os.environ["BCHOC_FILE_PATH"] = "bchoc_file.bin"
+
+file_path = os.environ["BCHOC_FILE_PATH"]
+# we need to implement these functions
+
+
+def append(case_id, item_list):
+    # get prehash value
+    bchoc_file_read = open(file_path, "rb")
+    data = bchoc_file_read.read()
+    index = 0
+    length = 0
+    while index <= (len(data) - 1):
+        # print(
+        #     datetime.datetime.fromtimestamp(
+        #         struct.unpack("d", data[index + 32 : index + 40])[0]
+        #     )
+        # )
+        length = struct.unpack("I", data[index + 72 : index + 76])[0]
+        index = index + length + 76
+        # print("len: ", length)
+        # print("index: ", index)
+    bchoc_file_read.close()
+    pre_sha256 = hashlib.sha256(data[index - length - 76 :]).hexdigest()
+    # print(pre_sha256)
+
+    bchoc_file = open(file_path, "ab")
+    print("Case: ", case_id)
+    for i in item_list:
+        pre_hash = bytes(pre_sha256, "utf-8")
+        dt = datetime.datetime.now()
+        time_stamp = dt.timestamp()
+        item_id = int(i)
+        state = bytes("CHECKEDIN", "utf-8")
+        data_length = 0
+        bchoc_file.write(
+            struct.pack(
+                "32sd16sI12sI",
+                pre_hash,
+                time_stamp,
+                bytes(case_id, "utf-8"),
+                item_id,
+                state,
+                data_length,
+            )
+        )
+        print("Added item: ", i)
+        print(
+            "  Status: CHECKEDIN\n  Time of action: ",
+            datetime.datetime.fromtimestamp(time_stamp),
+        )
+    bchoc_file.close()
+
+
 def checkout(item_id):
-	print(item_id)
+    print(item_id)
+
 
 def checkin(item_id):
-	print(item_id)
-	
-def log(num_entries,case_id,item_id,reverse):
-	#if num_entries is -1 then all log should be printed
-	bchoc_file=open("bchoc_file.bin","rb")
-	data=bchoc_file.read()
-	print(len(data))
-	index=0
-	while index<=(len(data)-1):
-		print(datetime.datetime.fromtimestamp(struct.unpack('d',data[index+32:index+40])[0]))
-		print(struct.unpack('32s',data[index:index+32])[0])
-		length=struct.unpack('I',data[index+72:index+76])[0]
-		index=index+length+76
-		print("len: ",length)
-		print("index: ",index)
-	print(num_entries)
-	print(case_id)
-	print(item_id)
-	print(reverse)
-	a=struct.unpack('d',data[32:40])
-	timestamp=datetime.datetime.fromtimestamp(a[0])
-	print(timestamp)
-	print(struct.unpack('I',data[72:76])[0])
-	bchoc_file.close()
+    print(item_id)
 
-def remove(item_id,reason,owner):
-	print(item_id)
-	print(reason)
-	print(owner)
-	
-	
+
+def log(num_entries, ip_case_id, ip_item_id, reverse):
+    # if num_entries is -1 then all log should be printed
+    num_entries = int(num_entries)
+    bchoc_file = open(file_path, "rb")
+    data = bchoc_file.read()
+    # print(len(data))
+    index = 0
+    listOfEntries = []
+    while index <= (len(data) - 1):
+        # pre hash value
+        # print(struct.unpack("32s", data[index : index + 32])[0])
+        dateTime = datetime.datetime.fromtimestamp(
+            struct.unpack("d", data[index + 32 : index + 40])[0]
+        )
+        case_id = (
+            str(struct.unpack("16s", data[index + 40 : index + 56])[0])
+            .split("\\x")[0]
+            .split("'")[1]
+        )
+        item_id = struct.unpack("I", data[index + 56 : index + 60])[0]
+        status = (
+            str(struct.unpack("12s", data[index + 60 : index + 72])[0])
+            .split("\\x")[0]
+            .split("'")[1]
+        )
+        length = struct.unpack("I", data[index + 72 : index + 76])[0]
+        index = index + length + 76
+        # print("len: ", length)
+        # print("index: ", index)
+        listOfEntries.append((case_id, item_id, status, str(dateTime)))
+        # print(
+        #     f"Case: {case_id}\nItem: {item_id}\nAction: {status}\nTime: {dateTime}\n\n"
+        # )
+    if reverse:
+        listOfEntries.reverse()
+    if num_entries == -1:
+        num_entries = len(listOfEntries)
+
+    for item in listOfEntries:
+        if num_entries == 0:
+            break
+        if ip_item_id and int(item[1]) != int(ip_item_id):
+            continue
+        case_id = item[0]
+        item_id = item[1]
+        status = item[2]
+        dateTime = item[3]
+        print(
+            f"Case: {case_id}\nItem: {item_id}\nAction: {status}\nTime: {dateTime}\n\n"
+        )
+        num_entries -= 1
+
+    # print("*************")
+    # print(num_entries)
+    # print(case_id)
+    # print(item_id)
+    # print(reverse)
+    # a = struct.unpack("d", data[32:40])
+    # timestamp = datetime.datetime.fromtimestamp(a[0])
+    # print(timestamp)
+    # print(struct.unpack("I", data[72:76])[0])
+    bchoc_file.close()
+
+
+def remove(item_id, reason, owner):
+    print(item_id)
+    print(reason)
+    print(owner)
+
+
 def init():
-	if(os.path.exists('bchoc_file.bin')==False):
-		print("Blockchain file not found. Create INITIAL block")
-		dt=datetime.datetime.now()
-		time_stamp=dt.timestamp()
-		print(time_stamp)
-		pre_hash=bytes("None",'utf-8')
-		case_id=bytes("None",'utf-8')
-		item_id=bytes("None",'utf-8')
-		state=bytes("INITIAL",'utf-8')
-		data_length=format(14,'b')
-		data=bytes("Initial block",'utf-8')
-		bchoc_file=open("bchoc_file.bin","ab")
-		bchoc_file.write(struct.pack('32sd16sI12sI',pre_hash,time_stamp,case_id,0,state,14))
-		bchoc_file.write(data)
-		bchoc_file.write(b'\0')
-		bchoc_file.close()
-	else:
-		print("Blockchain file found with INITIAL block")
-	
+    if os.path.exists(file_path) == False:
+        print("Blockchain file not found. Created INITIAL block.")
+        dt = datetime.datetime.now()
+        time_stamp = dt.timestamp()
+        # print(time_stamp)
+        pre_hash = bytes("None", "utf-8")
+        case_id = bytes("None", "utf-8")
+        item_id = bytes("None", "utf-8")
+        state = bytes("INITIAL", "utf-8")
+        data_length = format(14, "b")
+        data = bytes("Initial block", "utf-8")
+        bchoc_file = open(file_path, "ab")
+        bchoc_file.write(
+            struct.pack("32sd16sI12sI", pre_hash, time_stamp, case_id, 0, state, 14)
+        )
+        bchoc_file.write(data)
+        bchoc_file.write(b"\0")
+        bchoc_file.close()
+    else:
+        print("Blockchain file found with INITIAL block.")
+
+
 def verify():
-	print("verify")
-	
+    print("verify")
+
 
 # parse the input
 
 inputArray = sys.argv
+# print(sys.argv)
 if inputArray[0] == "./bchoc":
     # add commands
     if inputArray[1] == "add":
-        print("perform add command")
+        # print("perform add command")
         if inputArray[2] == "-c":
             case_id = inputArray[3]
-            #bchoc_file.write(bytes(case_id,'utf-8'))
+            # bchoc_file.write(bytes(case_id,'utf-8'))
             item_list = []
             for i in range(4, len(inputArray), 2):
-                #print("another item")
+                # print("another item")
                 if inputArray[i] == "-i":
                     item_list.append(inputArray[i + 1])
-                    #bchoc_file.write(inputArray[i + 1])
+                    # bchoc_file.write(inputArray[i + 1])
                 else:
                     sys.exit(1)
-            #print(item_list)
-            append(case_id,item_list) 
+            # print(item_list)
+            append(case_id, item_list)
         else:
-            sys.exit(1)     
+            sys.exit(1)
     # checkout command
     elif inputArray[1] == "checkout":
         print("perform checkout command")
@@ -141,29 +212,29 @@ if inputArray[0] == "./bchoc":
         else:
             sys.exit(1)
     elif inputArray[1] == "log":
-        num_entries=-1
-        case_id=""
-        item_id=""
-        reverse=False
+        num_entries = -1
+        case_id = ""
+        item_id = ""
+        reverse = False
         for i in range(len(inputArray)):
-            if(inputArray[i]=="-n"):
-                num_entries=inputArray[i+1]
-            if(inputArray[i]=="-c"):
-                case_id=inputArray[i+1]
-            if(inputArray[i]=="-i"):
-                item_id=inputArray[i+1]
-            if(inputArray[i]=="-r"):
-                reverse=True
-        log(num_entries,case_id,item_id,reverse)
+            if inputArray[i] == "-n":
+                num_entries = inputArray[i + 1]
+            if inputArray[i] == "-c":
+                case_id = inputArray[i + 1]
+            if inputArray[i] == "-i":
+                item_id = inputArray[i + 1]
+            if inputArray[i] == "-r":
+                reverse = True
+        log(num_entries, case_id, item_id, reverse)
     elif inputArray[1] == "remove":
         print("perform remove command")
         # call remove command here
-        item_id=inputArray[3]
-        reason=inputArray[5]
-        owner=""
-        if(len(inputArray)>6):
-        	owner=inputArray[7]
-        remove(item_id,reason,owner)
+        item_id = inputArray[3]
+        reason = inputArray[5]
+        owner = ""
+        if len(inputArray) > 6:
+            owner = inputArray[7]
+        remove(item_id, reason, owner)
     elif inputArray[1] == "init":
         # call the Linked list constructor to check the LL or create intial block
         init()
@@ -175,7 +246,5 @@ if inputArray[0] == "./bchoc":
         sys.exit(1)
 else:
     sys.exit(1)
-print(inputArray)
 
 # pack the data should be stored in bchoc
-
