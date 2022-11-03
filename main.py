58262a725d1420de318a213cae73b4a005dc8cb7
cult_mentality <kavitkarswanand5@gmail.com>
2022-11-03 01:54:28 -0700

    Added Checkin and Checkout with error codes

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ file_path = os.environ["BCHOC_FILE_PATH"]
 # we need to implement these functions
 
 
-def append(case_id, item_list):
+def append(case_id, item_list, ip_state="CHECKEDIN", message="Added item: "):
     # get prehash value
     bchoc_file_read = open(file_path, "rb")
     data = bchoc_file_read.read()
@@ -45,7 +45,7 @@ def append(case_id, item_list):
         dt = datetime.datetime.now()
         time_stamp = dt.timestamp()
         item_id = int(i)
-        state = bytes("CHECKEDIN", "utf-8")
+        state = bytes(ip_state, "utf-8")
         data_length = 0
         bchoc_file.write(
             struct.pack(
@@ -58,20 +58,64 @@ def append(case_id, item_list):
                 data_length,
             )
         )
-        print("Added item: ", i)
+        print(message, i)
         print(
-            "  Status: CHECKEDIN\n  Time of action: ",
+            f"  Status: {ip_state}\n  Time of action: ",
             datetime.datetime.fromtimestamp(time_stamp),
         )
     bchoc_file.close()
 
 
 def checkout(item_id):
-    print(item_id)
+    bchoc_file = open(file_path, "rb")
+    data = bchoc_file.read()
+    listOfEntries = create_listOfItems(data)
+    listOfEntries.reverse()
+    item = getItem(listOfEntries, item_id)
+    if not item:
+        print("Error: Checkout failed,item does not exist")
+        return 21
+    status = item[2]
+    case_id = item[0]
+    # Error codes :
+    # 2 -> checkout error
+    #   1 : does not exist error
+    #   2 : item already checkedout error
+    #   3 : item already removed error
+    if status:
+        if status in ["RELEASED", "DISPOSED", "DESTROYED"]:
+            print("Error: Cannot check out a removed item.")
+            return 23
+        elif status == "CHECKEDOUT":
+            print("Error: Cannot check out a checked out item. Must check it in first.")
+            return 22
+        else:
+            append(case_id, [item_id], "CHECKEDOUT", "Checked out item: ")
+            return 0
 
 
 def checkin(item_id):
-    print(item_id)
+    bchoc_file = open(file_path, "rb")
+    data = bchoc_file.read()
+    listOfEntries = create_listOfItems(data)
+    listOfEntries.reverse()
+    item = getItem(listOfEntries, item_id)
+    if not item:
+        print("Error: Checkin failed,item does not exist")
+        return 11
+    status = item[2]
+    case_id = item[0]
+    # Error codes :
+    # 1 -> checkin error
+    #   1 : does not exist error
+    #   2 : item already removed error
+    if status:
+        if status in ["RELEASED", "DISPOSED", "DESTROYED"]:
+            print("Error: Cannot check in a removed item.")
+            return 12
+        else:
+            append(case_id, [item_id])
+            return 0
 
 
 def log(num_entries, ip_case_id, ip_item_id, reverse):
@@ -80,33 +124,7 @@ def log(num_entries, ip_case_id, ip_item_id, reverse):
     bchoc_file = open(file_path, "rb")
     data = bchoc_file.read()
     # print(len(data))
-    index = 0
-    listOfEntries = []
-    while index <= (len(data) - 1):
-        # pre hash value
-        # print(struct.unpack("32s", data[index : index + 32])[0])
-        dateTime = datetime.datetime.fromtimestamp(
-            struct.unpack("d", data[index + 32 : index + 40])[0]
-        )
-        case_id = (
-            str(struct.unpack("16s", data[index + 40 : index + 56])[0])
-            .split("\\x")[0]
-            .split("'")[1]
-        )
-        item_id = struct.unpack("I", data[index + 56 : index + 60])[0]
-        status = (
-            str(struct.unpack("12s", data[index + 60 : index + 72])[0])
-            .split("\\x")[0]
-            .split("'")[1]
-        )
-        length = struct.unpack("I", data[index + 72 : index + 76])[0]
-        index = index + length + 76
-        # print("len: ", length)
-        # print("index: ", index)
-        listOfEntries.append((case_id, item_id, status, str(dateTime)))
-        # print(
-        #     f"Case: {case_id}\nItem: {item_id}\nAction: {status}\nTime: {dateTime}\n\n"
-        # )
+    listOfEntries = create_listOfItems(data)
     if reverse:
         listOfEntries.reverse()
     if num_entries == -1:
@@ -136,6 +154,44 @@ def log(num_entries, ip_case_id, ip_item_id, reverse):
     # print(timestamp)
     # print(struct.unpack("I", data[72:76])[0])
     bchoc_file.close()
+
+
+def create_listOfItems(data):
+    index = 0
+    listOfEntries = []
+    while index <= (len(data) - 1):
+        # pre hash value
+        # print(struct.unpack("32s", data[index : index + 32])[0])
+        dateTime = datetime.datetime.fromtimestamp(
+            struct.unpack("d", data[index + 32 : index + 40])[0]
+        )
+        case_id = (
+            str(struct.unpack("16s", data[index + 40 : index + 56])[0])
+            .split("\\x")[0]
+            .split("'")[1]
+        )
+        item_id = struct.unpack("I", data[index + 56 : index + 60])[0]
+        status = (
+            str(struct.unpack("12s", data[index + 60 : index + 72])[0])
+            .split("\\x")[0]
+            .split("'")[1]
+        )
+        length = struct.unpack("I", data[index + 72 : index + 76])[0]
+        index = index + length + 76
+        # print("len: ", length)
+        # print("index: ", index)
+        listOfEntries.append((case_id, item_id, status, str(dateTime)))
+        # print(
+        #     f"Case: {case_id}\nItem: {item_id}\nAction: {status}\nTime: {dateTime}\n\n"
+        # )
+    return listOfEntries
+
+
+def getItem(listOfEntries, item_id):
+    for item in listOfEntries:
+        if int(item[1]) == int(item_id):
+            return item
+    return None
 
 
 def remove(item_id, reason, owner):
@@ -196,21 +252,22 @@ if inputArray[0] == "./bchoc":
             sys.exit(1)
     # checkout command
     elif inputArray[1] == "checkout":
-        print("perform checkout command")
+        # print("perform checkout command")
         if len(inputArray) == 4 and inputArray[2] == "-i":
             item_id = inputArray[3]
             # call checkout command here
-            checkout(item_id)
-        else:
-            sys.exit(1)
+            exit_code = checkout(item_id)
+        if exit_code:
+            sys.exit(exit_code)
     elif inputArray[1] == "checkin":
         print("perform checkin command")
         if len(inputArray) == 4 and inputArray[2] == "-i":
             item_id = inputArray[3]
             # call checkin command here
-            checkin(item_id)
-        else:
-            sys.exit(1)
+            exit_code = checkin(item_id)
+            if exit_code:
+                sys.exit(exit_code)
+
     elif inputArray[1] == "log":
         num_entries = -1
         case_id = ""
